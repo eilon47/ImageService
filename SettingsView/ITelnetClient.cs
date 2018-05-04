ff3d6f819d23f6aa8c95b7e9ec9e5aa85e24ff15
daniel greenspan <greenspan1993@gmail.com>
2018-05-04 11:21:26 +0300

    fixed

---
+++ SettingsView/ITelnetClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsView
+{
+    interface ITelnetClient
+    {
+        void Connect(string ip, int port);
+        void Write(string command);
+        string Read(); // blocking call
+        void Disconnect();
+    }
+}
