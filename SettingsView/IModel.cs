0e743a52a4ddff368135e20d4e4e9bdf79a96af5
eilon47 <33837343+eilon47@users.noreply.github.com>
2018-05-04 11:11:03 +0300

    ds

---
+++ SettingsView/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsView
+{
+    interface IModel : INotifyPropertyChanged
+    {
+        void Connect(string ip, int port);
+        void Disconnect();
+        void Start();
+    }
+}
