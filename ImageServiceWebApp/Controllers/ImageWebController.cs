9ef0638d411719d92ac76fbcf7a5ad86e97589c4
elon bashari <elon.bashari@live.biu.ac.il>
2018-06-08 06:49:35 +0300

    sss

---
+++ ImageServiceWebApp/Controllers/ImageWebController.cs
@@ -0,0 +1,22 @@
+﻿using ImageServiceWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageServiceWebApp.Controllers
+{
+    public class ImageWebController : Controller
+    {
+        private static ImageWebModel model = new ImageWebModel();
+        // GET: ImageWeb
+        public ActionResult ImageWeb()
+        {
+            ViewBag.PhotosCounter = model.PhotosCounter;
+            ViewBag.Status = model.Status;
+            ViewBag.Students = model.Students;
+            return View(model);
+        }
+    }
+}
---
+++ ImageServiceWebApp/Controllers/LogsController.cs
@@ -0,0 +1,19 @@
+﻿using ImageServiceWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageServiceWebApp.Controllers
+{
+    public class LogsController : Controller
+    {
+        private static LogsModel model = new LogsModel();
+
+        public ActionResult Logs()
+        {
+            return View(model.Logs);
+        }
+    }
+}
